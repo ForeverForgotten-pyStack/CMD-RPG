183dcc923abc37425457b191323c47d80674fb1c
ForeverForgotten-pyStack <hopelesslyforgotten@outlook.com>
2024-11-08 13:48:50 -0600

    Create main.py

---
+++ ActualGame/main.py
@@ -0,0 +1,28 @@
+## Main imports for my code
+import time, os, sys
+from os import system, name
+
+# Renaming of the ones used most often
+from time import sleep as s
+import random as r
+
+# All of the defines
+
+def clear():
+  pass
+
+def logo():
+  pass
+
+def lsn():
+  #login/signups scr
+  pass
+
+def main():
+  pass
+
+def save():
+  pass
+
+def load():
+  pass
