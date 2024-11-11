3d3f3e0f8f53f31a4aa6f788eedfa4370351ff9b
ForeverForgotten-pyStack <hopelesslyforgotten@outlook.com>
2024-11-10 19:29:52 -0800

    debuged

--- ActualGame/main.py
+++ ActualGame/main.py
@@ -33,25 +33,24 @@ def logo():
   print("\n\n")
 
 def lsn():
-  logo()
-  userinp = int(input("Have you played before (Do you have save data) Yes (1) or No (2)"))
+    logo()
+    userinp = int(input("Have you played before (Do you have save data) Yes (1) or No (2)"))
+    if userinp == 1:
+        print("Loging in...")
+        sleep(2)
+        loading()
+        login()
 
-if userinp == 1:
-  print("Loging in...")
-  sleep(2)
-  loading()
-  login()
+    elif userinp == 2:
+      print("Lets get you signed up!")
+      sleep(2)
+      loading()
+      signup()
 
-elif userinp == 2:
-  print("Lets get you signed up!")
-  sleep(2)
-  loading()
-  signup()
-
-else:
-  print("That is not a valid option, the Valid options are 1 and 2, plase try again.")
-  sleep(2)
-  lsn()
+    else:
+      print("That is not a valid option, the Valid options are 1 and 2, plase try again.")
+      sleep(2)
+      lsn()
 
 def main():
   pass
